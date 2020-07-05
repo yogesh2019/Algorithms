681c329fc60ba6b070a689ca07f3702a1ee75a47
Yogesh Khichi <yogeshkkhichi@gmail.com>
2020-07-05 09:10:40 +0530

    finding the middle element

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,5 +1,7 @@
-// changing for the test
-
+// firstly the array is given as parameter to the function binarySearch
+// it compares the searchElement to the middleElement of the array
+// if it matches it returns the middle element
+// first lets try this step
 public class BinarySearch {
     // the time comp is = O(n)
     public static void main(String args[])
@@ -7,33 +9,17 @@ public class BinarySearch {
         BinarySearch B = new BinarySearch();
         int arr[] = {2,3,4,9,11,10,14};
         int n = arr.length;
-        int result = B.binarySearch(arr,0,n-1,10);
+        int result = B.binarySearch(arr,0,n-1,9);
         System.out.println(result);
     }
     int binarySearch(int arr[],int l , int r, int x)
     {
-        for(int i = l ;i <=r ; i++)
-        {
-            System.out.print(arr[i]+",");
-        }
-        System.out.println();
-      if(r>=l)
-      {
-          int mid = l + (r-l)/2;
-          System.out.println(mid);
-
-          if(arr[mid] == x)
-            return mid;
-
-         if(arr[mid]>x)
-         {
-             binarySearch(arr, l, mid-1, x);
-         }
-
-         return binarySearch(arr, mid+1, r, x);
-          
-      }
-      return -1;
+        int mid = (l+r)/2;
+       
+        if(arr[mid] == x)
+         return mid;
+        return 0;
+       // firstly let return the integer to satisfy the function
     }
     
 }
